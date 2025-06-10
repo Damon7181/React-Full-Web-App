db517736d4b4f3e07677fb91a793754bc8d63a86
Ubaid ur Rehman <damon7181@gmail.com>
2025-06-10 12:04:07 +0500

    Components created to make JinnByte eshop admin portal 13(Product schema changed)

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -3,8 +3,12 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
   name: String,
   image: String,
+  url: String,
   price: String,
   originalPrice: String,
+  maxTemperature: String,
+  material: String,
+  emfEmission: String,
   status: String,
   views: Number,
 });
